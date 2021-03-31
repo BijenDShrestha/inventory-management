5850526986483a998340fd5feafb1ed2ca048750
bijendshrestha <bijenshrestha09@gmail.com>
2021-04-01 00:18:29 +0545

    search added

--- inventory-management/Controllers/InventoryController.cs
+++ inventory-management/Controllers/InventoryController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 using InventoryManagement.Models;
 using InventoryManagement.Data;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Controllers{
     public class InventoryController: Controller{
@@ -83,5 +86,38 @@ namespace InventoryManagement.Controllers{
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult Sell(int Id)
+        {
+            var abcdef = db.Inventories.Find(Id);
+            return View(abcdef);
+        }
+
+
+        [HttpPost]
+        public ActionResult Sell(Inventory inventory)
+        {
+            db.Inventories.Attach(inventory);
+            db.Inventories.Update(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }    
+
+        public async Task<IActionResult> Search(string searchString)
+        {
+            var goods = from m in db.Inventories
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                goods = goods.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(await goods.ToListAsync());
+        }
+
+
+
     }
 }
